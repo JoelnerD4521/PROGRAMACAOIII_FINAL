e95f6709c8dae1242d64948704ef0071ae48518a
Jose Fernandes <122645810+JoelnerD4521@users.noreply.github.com>
2025-01-10 15:41:11 +0100

    Add files via upload

---
+++ Apartamento.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author joelf
+ */
+public class Apartamento {
+    String tipoApartamento;
+    int numeroCasaBanho;
+    int numeroVarandas;
+    boolean temTerraco;
+    
+}
---
+++ Proprietario.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Classes;
+
+/**
+ *
+ * @author joelf
+ */
+public class Proprietario {
+    int idProprietario;
+    String nome;
+    String morada;
+    String telefone;
+    String email;
+    String data_nascimento;
+    
+}
